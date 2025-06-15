2c1c8a1716d401ab0318e9f0a5c1be77d5f62134
iaorana-ora-pf <PaanaConseil@outlook.com>
2025-06-14 14:38:30 -1000

    Update explorer.js

--- explorer.js
+++ explorer.js
@@ -135,7 +135,7 @@ const total = Object.values(events).flat().filter(e =>
 
 const countEl = document.getElementById("event-count-display");
 if (countEl) {
-  countEl.innerHTML = `Découvrez l'histoire de la santé publique à travers <strong>${total}</strong> événements clés.`;
+  countEl.innerHTML = `Explorez, filtrez et plongez dans les détails de <strong>${total}</strong> événements de santé publique en Polynésie française .`;
 }
 }
 
